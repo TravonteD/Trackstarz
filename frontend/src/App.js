808875c0eabd92a1ff8b61d1cb7bed5bd926d75f
TravonteD <tman1300@aol.com>
2020-01-24 20:11:55 -0500

    start frontend over and add sass

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './App.css';
 
-class App extends Component {
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+class Header extends React.Component {
   render() {
     return (
-      <div className="App">
-      <p>Hello World</p>
-      </div>
-    );
+      <header className="trackstarz__header">
+      <Menu/>
+      </header>
+    )
+  }
+}
+
+class Menu extends React.Component {
+  render() {
+    return (
+      <nav className="trackstarz__header__nav">
+        <li className="trackstarz__header_nav_item">Home</li>
+        <li>About</li>
+        <li>Universe</li>
+      </nav>
+    )
   }
 }
 
